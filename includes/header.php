23ffd2138283cd28a11b63c68540fc34e1bf0f54
Yunus (CODEGA) <yunus@codega.com.tr>
2026-04-26 06:27:03 +0000

    v1.0.1: includes/header.php require yolu düzeltildi
    includes/header.php satır 4 hatalı şekilde __DIR__ . '/includes/functions.php'
    arıyordu, ancak header.php zaten includes/ içinde olduğu için bu
    includes/includes/functions.php yoluna çıkıyordu. Düzeltildi:
    __DIR__ . '/functions.php'
    
    Hata raporu:
      Failed to open stream: includes/includes/functions.php

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/includes/functions.php';
+require_once __DIR__ . '/functions.php';
 
 $page_slug   = $page_slug   ?? 'home';
 $page_title  = $page_title  ?? setting('site_name', SITE_NAME);
